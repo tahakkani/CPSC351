8e82287abdaeed72b576c2f770c76d2b256d3d0a
tahakkani <38796045+tahakkani@users.noreply.github.com>
2019-12-06 09:50:16 -0800

    Add files via upload

--- proj8/proj8.py
+++ proj8/proj8.py
@@ -16,55 +16,60 @@ class DPDA:
         self.stack = ['']
     def __reset(self):
         self.currSt = self.q0
+        self.stack = ['']
+    #input: a string to be determined if it is in the language
+    #output: prints Accept or Reject
     def __read(self, input):
-        for x in input:
-            print(self.currSt, " ", self.stack, "\n")
-            #check to see if there are any e-transitions in current state, if yes, then fire them up, skip to next statement
-            self.checkForETransitions()
-            for k2 in self.delta:
-                if k2 == (self.currSt, x, ''):
-                    self.currSt = self.delta[k2][0]
-                    if not self.delta[k2][1] == '':
-                        self.stack.append(self.delta[k2][1])
-                    more = False
-                    break
-                if k2 == (self.currSt, x, self.stack[len(self.stack) - 1]):
-                    self.currSt = self.delta[k2][0]
-                    self.stack.pop()
-                    if not self.delta[k2][1] == '':
-                        self.stack.append(self.delta[k2][1])
-                    more = False
-                    break
-        self.checkForETransitions()
+        for a in input:
+            noMoves = False #noMoves lets the while loop know 
+            while True:
+                if not self.executeMove(a):
+                    if not self.executeMove(''):
+                        noMoves = True   #this break needs to break the for loop
+                        break
+                    continue    #this hits when e transition happens
+                break   #this break exits while loop so that we can continue to next character
+            if noMoves:
+                break
+        if input:   #this happens only if the string is null. Allows for final e transition before determining input
+            self.executeMove('')
         if self.currSt in self.F:
             print('ACCEPT\n')
         else:
             print('REJECT\n')
-    def checkForETransitions(self):
-        more = True #more keeps track of if there might be any more e-transitions for the current state
-        while more:
-            more = False
-            for k in self.delta:
-                if k == (self.currSt, '', self.stack[len(self.stack) - 1]):
-                    self.currSt = self.delta[k][0]
-                    if not self.delta[k][1] == '':
-                        self.stack.append(self.delta[k][1])
-                    more == True
-                    break
+    #input: char is next character to be read, either an element of alphabet or e
+    #output: returns boolean indicating wether or not a move was made
+    def executeMove(self, char):
+        worked = False
+        for k in self.delta:
+            if k[0] == self.currSt and  k[1] == char:
+                if k[2] == self.stack[-1]:
+                    self.stack.pop()
+                elif not k[2] == '':
+                    continue
+                self.currSt = self.delta[k][0]
+                if not self.delta[k][1] == '':
+                    self.stack.append(self.delta[k][1])
+                worked = True
+                break
+            else:
+                continue
+        return worked
 
 
-
-
-states = {'A', 'B', 'C', 'D'}
+states = {'A', 'B', 'C', 'D', 'R'}
 alpha = {'0','1'}
 stackAlpha = {'0', '1', '$'}
 delta = {('A', '', ''):('B', '$'),
          ('B', '0', ''):('B', '0'),
          ('B','1', '0'):('C', ''),
          ('C','1', '0'):('C', ''),
+         ('C','1', '$'):('R', ''),
+         ('C', '0', '0'):('R', ''),
+         ('C', '0', '$'):('R', ''),
          ('C','', '$'):('D', '')}
 start = 'A'
-accepts = {'D'}
+accepts = {'D', 'A'}
 myDPDA = DPDA(states, alpha, stackAlpha, delta, start, accepts)
 
 cont = True
@@ -73,4 +78,4 @@ while cont:
     myDPDA._DPDA__read(testString)
     myDPDA._DPDA__reset()
     choice = input("Continue? (y/n): ")
-    if choice == 'n' or 'N': cont = False
+    if choice == 'n': cont = False
